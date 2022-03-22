4afcb2339b1d45838b9cebb686caf5c8e263316d
Cristian Tulbu <96737712+Cristian6918@users.noreply.github.com>
2022-03-22 16:26:01 +0100

    Snack3 done

--- snack3/script.js
+++ snack3/script.js
@@ -1,1 +1,17 @@
 console.log('snack3 ok');
+
+// Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
+
+
+let string = prompt('Inserire una string!');
+//Function that Reverse the imput string;
+function reverseString(string) {
+    let reverseString = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        reverseString += string[i];
+    }
+    string = reverseString;
+    return string;
+}
+
+alert(reverseString(string));
